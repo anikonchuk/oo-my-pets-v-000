f0e77f1a18af1481dc548e5191ccf00d4fe31e87
Andrew Nikonchuk <anikonch@gmail.com>
2018-06-22 15:48:21 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,7 +1,13 @@
 class Owner
   attr_accessor :name 
+  attr_reader :species
   
   @@all = []
+  
+  def initialize(species)
+    @species = species
+    self.class.all << self 
+  end
   
   def self.all 
     @@all
